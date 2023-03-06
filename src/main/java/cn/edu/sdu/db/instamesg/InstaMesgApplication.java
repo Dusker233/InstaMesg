ce2726ee35b823400c2105221a8fa5c217e8d056
Dusker233 <1187305740@qq.com>
2023-03-06 20:10:36 +0800

    Initial commit

---
+++ src/main/java/cn/edu/sdu/db/instamesg/InstaMesgApplication.java
@@ -0,0 +1,13 @@
+package cn.edu.sdu.db.instamesg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstaMesgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstaMesgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cn/edu/sdu/db/instamesg/InstaMesgApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.edu.sdu.db.instamesg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstaMesgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
